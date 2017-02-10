e50dd05f42322cd1c04e8481be683ad1b8f0ca33
widha <widha@m2m.com>
2017-02-10 13:10:01 +0700

    Add publish topic

--- src/main/java/io/iotera/emma/smarthome/utility/PublishUtility.java
+++ src/main/java/io/iotera/emma/smarthome/utility/PublishUtility.java
@@ -1,9 +1,48 @@
 package io.iotera.emma.smarthome.utility;
 
+import io.iotera.util.Text;
+
 public class PublishUtility {
 
     public static String topic(String baseTopic, long accountId, String type) {
         return baseTopic + '/' + accountId + '/' + type;
     }
+
+    public static String topicHub(long accountId, String type, String id) {
+        String topic = "hub/" + accountId + "/" + type;
+        if (!Text.isEmpty(id)) {
+            topic += "/" + id;
+        }
+        return topic;
+    }
+
+    public static String topicHub(long accountId, String type) {
+        return topicHub(accountId, type, null);
+    }
+
+    public static String topicClient(long clientId, String type, String id) {
+        String topic = "client/" + clientId + "/" + type;
+        if (!Text.isEmpty(id)) {
+            topic += "/" + id;
+        }
+        return topic;
+    }
+
+    public static String topicClient(long clientId, String type) {
+        return topicClient(clientId, type, null);
+    }
+
+    public static String topicClientHub(long accountId, String type, String id) {
+        String topic = "client/hub/" + accountId + "/" + type;
+        if (!Text.isEmpty(id)) {
+            topic += "/" + id;
+        }
+        return topic;
+    }
+
+    public static String topicClientHub(long accountId, String type) {
+        return topicClientHub(accountId, type, null);
+    }
+
 
 }
